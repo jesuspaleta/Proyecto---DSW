ba561ad6c64a1176c22eb93437fa86181e524429
jesuspaleta <jesupaleta@gmail.com>
2015-11-03 13:35:56 -0600

    Create about.php

---
+++ about.php
@@ -0,0 +1,36 @@
+<html>
+  
+  <head>
+    <title>EverySpeak</title>
+    <link rel="stylesheet" type="text/css" href="style.css"  />
+
+  </head>
+  
+  <body>
+    <div id = 'head'>
+      <div id = 'tit'>EverySpeak</div>
+      
+      <div id = 'paginas'>
+        <a href="index.php"><div class = 'liga'>Inicio</div></a>
+        <a href="registro.php"><div class = 'liga'>Registro</div></a>
+        <a href="idiomas.php"><div class = 'liga'>Idiomas</div></a>
+        <a href="contacto.php?id=0"><div class = 'liga'>Contacto</div></a>
+        <a href="about.php"><div class = 'liga'>About</div></a>
+        
+      </div>
+    </div>
+    
+    <div id = 'cont'>
+      <div id = 'reg'> <br>
+        <h2>EverySpeak es una aplicacion web  que te permite aprender
+            otro idioma <br> al mismo tiempo que tu enseñas otro.</h2>
+        <h2>Con EverySpeak puedes comunicarte con hablantes nativos de mas de<br> 5 Idiomas
+            como : Ingles, Espanol, Frances, Aleman y Chino Mandarin. <h2>
+          
+      </div>
+      
+    </div>
+    <div id = 'footer'>Copyright || Jesus Paleta, Liam Morales</div>
+  </body>
+  
+</html>
